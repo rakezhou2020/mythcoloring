63a44b2b10ae054be044d5f80df7e9e8cb516b50
rakezhou2020 <rakezhou2020@gmail.com>
2026-09-10 13:50:00 +0800

    Add last modified dates to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,6 +4,7 @@ import { publishedCreatures } from "../data/creatures";
 import { siteUrl } from "../lib/seo";
 export const dynamic = "force-static";
 export default function sitemap(): MetadataRoute.Sitemap {
+  const lastModified = new Date().toISOString().slice(0, 10);
   return [
     "/",
     "/coloring-pages/",
@@ -15,5 +16,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
     "/contact/",
     ...publishedThemes.map((t) => "/themes/" + t.slug + "/"),
     ...publishedCreatures.map((c) => "/creatures/" + c.slug + "/"),
-  ].map((path) => ({ url: siteUrl + path }));
+  ].map((path) => ({ url: siteUrl + path, lastModified }));
 }
